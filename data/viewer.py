a815595e0e6923aacfb6426af9c6834f7a984205
Spencer Imbleau <spencer@imbleau.com>
2021-09-01 17:06:35 -0400

    Added viewer

---
+++ data/viewer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import sys
+import matplotlib.pyplot as plt
+
+if len(sys.argv) != 2:
+    print("Please enter a file, i.e. `python view.py file.tiff`")
+    sys.exit(1)
+
+path = sys.argv[1]
+print(path)
+x = plt.imread(path)
+plt.imshow(x)
+plt.colorbar()
+plt.show()
