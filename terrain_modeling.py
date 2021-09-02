9cc48cb07a4b760e2020eedf65a0a41ac02c4d38
Spencer Imbleau <spencer@imbleau.com>
2021-09-02 18:16:22 -0400

    0.1 on WebCAT

--- terrain_modeling.py
+++ terrain_modeling.py
@@ -1,19 +1,40 @@
 # Pseudo code for the program operation:
+from helper_methods import *
+
+def run():
+    """
+    An minimal example training a linear regression model on the elevation data.
+
+    :return: None
+    """
+    from tensorflow.keras.models import Sequential
+    from tensorflow.keras.layers import Input, Dense, Lambda
+
+    output_path = 'terrain/linear'
+    tiff_file = f'terrain/Appalachian_State_0.1deg.tiff'
+
+    model_checkpoint = setup_model_checkpoints(output_path, save_freq='epoch')
+    x, y = get_xy(tiff_file)
+
+    keras.backend.clear_session()
+
+    # linear regression
+    model = Sequential()
+    model.add(Input(2))
+    model.add(Dense(1, activation='linear'))
+    # Initially the network outputs values centered at zero
+    # Add the mean elevation to start near the solution
+    y_mean = y.mean()
+    model.add(Lambda(lambda v: v + y_mean))
+
+    model.compile(loss='mean_squared_error', optimizer='adam', metrics=[Entropy()])
+    model.summary()
+
+    print_error(y, y.mean(), 1, 'Constant')
+
+    model.fit(x, y, batch_size=64, verbose=1, callbacks=[model_checkpoint])
+    compare_images(model, x, y, output_path)
 
 
-# Get file path for image TIFF
-import sys
-args = sys.argv
-if len(args) != 2:
-    print("Enter a file name.")
-    sys.exit(1)
-data_path = args[1]
-
-# Create and train a new model instance.
-model = create_model(data_path)
-model.fit(...)
-
-# Save the entire model to a HDF5 file.
-# The '.h5' extension indicates that the model should be saved to HDF5.
-# More here: https://www.tensorflow.org/tutorials/keras/save_and_load
-model.save(data_path + '.h5')
+if __name__ == '__main__':
+    run()
