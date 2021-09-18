0580e685c53c4b2490fd404f4b5922e09f46e0fa
Matt Sterckx <sterckx.matthias@live.com>
2021-09-18 14:38:38 -0400

    Changed loop to do all permutations for a file before starting on the next file

--- grid_search.py
+++ grid_search.py
@@ -34,24 +34,26 @@ if __name__ == '__main__':
     permutations = True
 
     if permutations:
-        for loss_function in loss_functions:
-            print("le")
-            for layer_count in layer_counts:
-                print(layer_count)
-                for perm in itertools.permutations(neuron_counts, layer_count):
-                    layers = []
-                    for neuron_amt in perm:
-                        layers.append(Dense(neuron_amt, activation='relu'))
-                    run(files, layers, loss_function,
-                        optimizer, batch_size, epochs, False)
+        for file in files:
+            for loss_function in loss_functions:
+                print("le")
+                for layer_count in layer_counts:
+                    print(layer_count)
+                    for perm in itertools.permutations(neuron_counts, layer_count):
+                        layers = []
+                        for neuron_amt in perm:
+                            layers.append(Dense(neuron_amt, activation='relu'))
+                        run([file], layers, loss_function,
+                            optimizer, batch_size, epochs, False)
 
     else:
-        for loss_function in loss_functions:
-            for layer_count in layer_counts:
-                for neuron_count in neuron_counts:
-                    # Create layers
-                    layers = []
-                    for i in range(layer_count):
-                        layers.append(Dense(neuron_count, activation='relu'))
-                    run(files, layers, loss_function,
-                        optimizer, batch_size, epochs, False)
+        for file in files:
+            for loss_function in loss_functions:
+                for layer_count in layer_counts:
+                    for neuron_count in neuron_counts:
+                        # Create layers
+                        layers = []
+                        for i in range(layer_count):
+                            layers.append(Dense(neuron_count, activation='relu'))
+                        run([file], layers, loss_function,
+                            optimizer, batch_size, epochs, False)
